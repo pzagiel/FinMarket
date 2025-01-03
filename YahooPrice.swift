9fd6705a3d493c4b85353bdd4aa151b016a628a6
Patrick Zagiel <patrick.zagiel@gmail.com>
2025-01-03 11:08:03 +0100

    add some debugging trace

--- YahooPrice.swift
+++ YahooPrice.swift
@@ -51,6 +51,7 @@ class YahooPrice: ObservableObject{
     func getPrice(ticker:String){
         print("Get Price call")
         let url = URL(string: "https://query1.finance.yahoo.com/v8/finance/chart/"+ticker+"?interval=1d&range=1d")!
+        print(url)
         let task = URLSession.shared.dataTask(with: url, completionHandler: getData)
         
     func getData(data: Data?, response: URLResponse?, error: Error?) {
@@ -58,37 +59,37 @@ class YahooPrice: ObservableObject{
         //  self.priceData = String(data: data, encoding: .utf8)!
       
         
-        do {
-            let  yahooPriceResult = try JSONDecoder().decode(YahooPriceResult.self, from: data)
-            yahooPriceResult1=yahooPriceResult
-            let resultMeta=yahooPriceResult.chart.result[0].meta
-            let priceValue1=(yahooPriceResult1?.chart.result[0].meta.regularMarketPrice)!
-            //priceValue=resultMeta.regularMarketPrice
-            //let prevPrice=resultMeta.chartPreviousClose
-            //priceEvol=((priceValue-prevPrice)/prevPrice)*100
-            
-            // Sleep tometime to analyse and debug
-            //let randomDelay = Int.random(in: 3...10)
-            //sleep(UInt32(randomDelay))
-            
-            
-            // Ensure synchro with main thread -change nothing it seems
-            DispatchQueue.main.async {
-                        
-                        print("Get Data from request")
-                           // Mettre à jour les propriétés sur la file principale
-                           self.priceValue = resultMeta.regularMarketPrice
-                           let prevPrice = resultMeta.chartPreviousClose
-                           self.priceEvol = ((priceValue1 - prevPrice) / prevPrice) * 100
-                           print(ticker + ":" + String(self.priceValue))
-                           self.objectWillChange.send()
-                           
-                       }
-            
-            
-        } catch let jsonError as NSError {
-            print("JSON decode failed: \(jsonError.localizedDescription)")
-        }
+            do {
+                let  yahooPriceResult = try JSONDecoder().decode(YahooPriceResult.self, from: data)
+                yahooPriceResult1=yahooPriceResult
+                let resultMeta=yahooPriceResult.chart.result[0].meta
+                let priceValue1=(yahooPriceResult1?.chart.result[0].meta.regularMarketPrice)!
+                //priceValue=resultMeta.regularMarketPrice
+                //let prevPrice=resultMeta.chartPreviousClose
+                //priceEvol=((priceValue-prevPrice)/prevPrice)*100
+                
+                // Sleep tometime to analyse and debug
+                //let randomDelay = Int.random(in: 3...10)
+                //sleep(UInt32(randomDelay))
+                
+                
+                // Ensure synchro with main thread -change nothing it seems
+                DispatchQueue.main.async {
+                            
+                            print("Get Data from request")
+                               // Mettre à jour les propriétés sur la file principale
+                               self.priceValue = resultMeta.regularMarketPrice
+                               let prevPrice = resultMeta.chartPreviousClose
+                               self.priceEvol = ((priceValue1 - prevPrice) / prevPrice) * 100
+                               print(ticker + ":" + String(self.priceValue))
+                               self.objectWillChange.send()
+                               
+                           }
+                
+                
+            } catch let jsonError as NSError {
+                print("JSON decode failed: \(jsonError.localizedDescription)")
+            }
     }
     task.resume()
     }
